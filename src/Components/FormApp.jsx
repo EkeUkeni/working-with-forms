6fd522bf776802873426ead45cb23c7fc10d576f
ekeukeni <ukenieke@gmail.com>
2023-05-02 18:36:56 +0100

    updated

--- src/Components/FormApp.jsx
+++ src/Components/FormApp.jsx
@@ -6,7 +6,7 @@ const FormApp = () => {
   return (
     <div>
         <Header heading = {'Welcome to Skill Tech website!'}/>
-        <Header  text = {'Become an expert in the highiest demanding Tech Skills in 2023. Level up your career goals faster than you thought. Feel free to contact us 24/7...'}/>
+        <Header  text = {'Become an expert in the highest demanding Tech Skills in 2023. Level up your career goals faster than you thought. Feel free to contact us 24/7...'}/>
         <h1>Contact Us</h1>
         <Forms/>
     </div>
